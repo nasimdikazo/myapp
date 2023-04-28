190d2c7e15610b3cd09d1ce67407f092d53f3f8a
Nasim Ansari <nasimansari@dikazo.com>
2023-04-28 17:38:46 +0530

    copyright added in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,6 +133,9 @@
                     <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
+                    <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                        Copyright @ Nasim Ansari
+                    </div>
                 </div>
             </div>
         </div>
